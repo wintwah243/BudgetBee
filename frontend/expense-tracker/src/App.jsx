967d12e76978ed507bcbdc39a42a0e8df1ce0d4e
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-16 17:32:21 +0630

    Update App.jsx

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -10,7 +10,7 @@ import SignUp from "./pages/Auth/SignUp";
 import Home from "./pages/Dashboard/Home";
 import Income from "./pages/Dashboard/Income";
 import Expense from "./pages/Dashboard/Expense";
-import UserProvider from './context/userContext';
+import UserProvider from './context/UserContext';
 import PasswordReset from './components/PasswordReset';
 import ForgotPassword from './components/ForgotPassword';
 import { Toaster } from "react-hot-toast";
