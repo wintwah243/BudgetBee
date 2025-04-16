0ff0458bf000211d1032ec5820c19f4195f44f80
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-16 21:49:38 +0630

    Update Login.jsx

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -74,7 +74,7 @@ const Login = () => {
   };
 
   const loginWithGoogle = () => {
-    window.open("https://budgetbee-backend-p6tn.onrender.com/api/v1/auth/google/callback", "_self");
+    window.open("https://budgetbee-backend-p6tn.onrender.com/api/v1/auth/google", "_self");
   };
 
   return (
