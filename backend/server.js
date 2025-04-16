ae2fd357e15a9ab300dac1697e6fe80781ffebee
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-17 01:58:54 +0630

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -90,15 +90,6 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname,"uploads")));
 
-// Serve static files from Vite's build
-app.use(express.static(path.join(__dirname, '../frontend/expense-tracker/dist')));
-
-// Serve index.html for all frontend routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/expense-tracker/dist/index.html'));
-});
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
