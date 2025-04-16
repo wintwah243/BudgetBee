345ced279f5525d827ed044a4b86aded6f0cc9af
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-17 02:03:12 +0630

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -90,6 +90,14 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname,"uploads")));
 
+app.use((req, res, next) => {
+  if (req.url.endsWith('.css')) {
+    res.setHeader('Content-Type', 'text/css');
+  }
+  next();
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
