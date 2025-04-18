9e535aa52e5a8662c8a79b56dad35a079e20fc0b
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-18 22:46:56 +0630

    Update uploadMiddleware.js

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,23 +1,15 @@
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-});
+const multer = require("multer");
+const { storage } = require("../config/cloudinary"); 
 
 const fileFilter = (req, file, cb) => {
     const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-    if(allowedTypes.includes(file.mimetype)){
+    if (allowedTypes.includes(file.mimetype)) {
         cb(null, true);
-    }else{
+    } else {
         cb(new Error('Only .jpeg, .jpg and .png formats are allowed.'), false);
     }
 };
 
-const upload = multer({storage, fileFilter});
+const upload = multer({ storage, fileFilter }); 
 
 module.exports = upload;
