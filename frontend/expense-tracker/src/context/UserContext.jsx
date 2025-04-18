0128e71c2aa68aa9909f25de127fc18687fb4b33
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-18 16:23:38 +0630

    Update UserContext.jsx

--- frontend/expense-tracker/src/context/UserContext.jsx
+++ frontend/expense-tracker/src/context/UserContext.jsx
@@ -1,27 +1,35 @@
-import React, {Children, createContext, useState} from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
-    //Function to update user data
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
     const updateUser = (userData) => {
         setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
     };
 
     //Function to clear user data (e.g logout)
     const clearUser = () => {
         setUser(null);
+        localStorage.removeItem("user");
     };
 
     return (
         <UserContext.Provider
-        value={{
-            user,
-            updateUser,
-            clearUser
-        }}
+            value={{
+                user,
+                updateUser,
+                clearUser
+            }}
         >
             {children}
         </UserContext.Provider>
