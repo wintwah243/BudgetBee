f8de83f7e98e6b155fd8da427ba19bc5f638f868
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-18 17:15:26 +0630

    Update SideMenu.jsx

--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { SIDE_MENU_DATA } from '../../utils/data'
 import { UserContext } from '../../context/UserContext'
 import { useNavigate } from 'react-router-dom';
