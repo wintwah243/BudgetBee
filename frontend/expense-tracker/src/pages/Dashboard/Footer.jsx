2d286dbaf1cbdc1046ede680aec1c8e9f3733889
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-18 09:54:10 +0630

    Update Footer.jsx

--- frontend/expense-tracker/src/pages/Dashboard/Footer.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
     <footer className="bg-purple-100 text-white py-10 px-4" style={{marginTop:'22px'}}>
       <div className="max-w-[1100px] mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-sm">
-          {/* About */}
           <div>
             <h3 className="text-yellow-400 font-semibold text-lg mb-2">BudgetBee</h3>
             <p className="text-gray-800">
@@ -14,7 +13,6 @@ const Footer = () => {
             </p>
           </div>
 
-          {/* Quick Links */}
           <div>
             <h4 className="text-yellow-400 font-semibold mb-2">Quick Links</h4>
             <ul className="space-y-2">
@@ -25,7 +23,6 @@ const Footer = () => {
             </ul>
           </div>
 
-          {/* Contact */}
           <div>
             <h4 className="text-yellow-400 font-semibold mb-2">Get in Touch</h4>
             <ul className="space-y-3">
@@ -33,8 +30,7 @@ const Footer = () => {
                 <FiMail className='text-gray-800'/>
                 <a
                   href="mailto:wahwint72@gmail.com"
-                  className="text-gray-800 hover:text-yellow-300 transition-colors"
-                >
+                  className="text-gray-800 hover:text-yellow-300 transition-colors">
                   wahwint72@gmail.com
                 </a>
               </li>
@@ -44,8 +40,7 @@ const Footer = () => {
                   href="https://github.com/wintwah243"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-gray-800 hover:text-yellow-300 transition-colors"
-                >
+                  className="text-gray-800 hover:text-yellow-300 transition-colors">
                   GitHub
                 </a>
               </li>
@@ -55,8 +50,7 @@ const Footer = () => {
                   href="http://linkedin.com/in/wint-wah-386240307"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-gray-800 hover:text-yellow-300 transition-colors"
-                >
+                  className="text-gray-800 hover:text-yellow-300 transition-colors">
                   LinkedIn
                 </a>
               </li>
@@ -64,7 +58,6 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Bottom Text */}
         <div className="mt-10 text-center text-gray-500 text-xs border-t border-gray-700 pt-4">
           © {new Date().getFullYear()} BudgetBee. All rights reserved.
         </div>
