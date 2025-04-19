fc613221b7edaf7c1e1baccb750bc0cd4cd1037c
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-19 08:34:14 +0630

    Update Income.js

--- backend/models/Income.js
+++ backend/models/Income.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const IncomeSchema = new mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     icon: {type: String},
-    source: {type: String, required: true}, //Income source job name
+    source: {type: String, required: true}, 
     description:{type:String},
     amount: {type: Number, required: true},
     date: {type: Date, default: Date.now},
